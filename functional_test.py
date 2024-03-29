352fdd019cbf7be108064c1b7fe06a2f65884c15
qiusu wang <susu@bupt.edu.cn>
2024-03-29 15:22:06 +0800

    msg 2: second commit

---
+++ functional_test.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(executable_path="C:/Program Files/Google/Chrome/Application/chromedriver")
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 张三听说有一个很酷的在线待办事项应用
+        # 他去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
+
+        # 他注意到网页的标题和头部都包含'To-Do'这个词
+        self.assertIn('To-Do', self.browser.title), "Browser title was " + self.browser.title
+        self.fail('Finish the test!') 
+
+        # 应用有一个输入代办事项的文本输入框
+
+        # 他在文本输入框中输入了'Buy flowers'
+
+        # 他按了回车键后，页面更新了
+        # 待办事项表格中显示了'1: Buy flowers'
+
+        # 页面中又显示了一个文本输入框，可以输入其他的待办事项
+        # 他输入了'Send a gift to Lisi'
+
+        # 页面再次更新，他的清单中显示了这两个待办事项
+
+        # 张三想知道这个网站是否会记住他的清单
+        # 他看到网站为他生成了一个唯一的URL
+
+        # 他访问这个URL，发现他的待办事项列表还在
+        # 他满意地离开了
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
