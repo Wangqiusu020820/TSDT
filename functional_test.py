b918d48ff0e725daea26ccdaec529c5da7816abd
qiusu wang <susu@bupt.edu.cn>
2024-04-12 15:05:03 +0800

    msg 6:Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+from selenium.webdriver.common.by import By
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -16,19 +19,34 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # 他注意到网页的标题和头部都包含'To-Do'这个词
-        self.assertIn('To-Do', self.browser.title), "Browser title was " + self.browser.title
-        self.fail('Finish the test!') 
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
         # 应用有一个输入代办事项的文本输入框
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # 他在文本输入框中输入了'Buy flowers'
+        inputbox.send_keys('Buy flowers')
 
         # 他按了回车键后，页面更新了
         # 待办事项表格中显示了'1: Buy flowers'
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Buy flowers' for row in rows)
+        )
 
         # 页面中又显示了一个文本输入框，可以输入其他的待办事项
         # 他输入了'Send a gift to Lisi'
-
+        self.fail('Finish the test!')
         # 页面再次更新，他的清单中显示了这两个待办事项
 
         # 张三想知道这个网站是否会记住他的清单
