49c4192f3f5081fb03c804af88c421bd52d71515
qiusu wang <susu@bupt.edu.cn>
2024-05-10 15:42:09 +0800

    msg 17: change models for multiple users

--- lists/tests.py
+++ lists/tests.py
@@ -3,10 +3,10 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
-class HomePageTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     # def test_root_url_resolves_to_home_page_view(self):
     #     found = resolve('/')
@@ -26,12 +26,17 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_saving_and_retrieving_items(self):
+        list_user = List()
+        list_user.save()
+
         first_item = Item()
         first_item.text = 'The first list item'
+        first_item.list = list_user
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_user
         second_item.save()
 
         saved_items = Item.objects.all()
@@ -40,7 +45,9 @@ class HomePageTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(first_saved_item.list, list_user)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_user)
 
     # def test_only_saves_items_when_necessary(self):
     #     self.client.get('/')
@@ -59,13 +66,9 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/lists/the-new-page/')
-
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
+        list_user = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_user)
+        Item.objects.create(text='itemey 2', list=list_user)
 
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-new-page/')
